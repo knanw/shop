df6654d39a09c4cb2102182e1f83410fc2dd2dc2
wolfram.knan.geekadmin <wolfram.knan@geekinbusiness.de>
2024-10-22 17:48:02 +0200

    Bugfix frontend edit, delete books

--- shop-frontend/components/Book.vue
+++ shop-frontend/components/Book.vue
@@ -1,117 +1,124 @@
 <template>
-  <v-progress-circular v-if="loading" indeterminate color="primary"></v-progress-circular>
-  <v-data-table v-else
-                fixed-header
-                density="compact"
-                :headers="headers"
-                item-key="item.id"
-                :items="items"
-                @click:row="goToDetails">
+  <v-sheet width="500" class="mx-auto">
+    <h2>Buch</h2>
+    <v-form ref="form">
+      <v-text-field v-if="isEditing" v-model="localForm.id" label="ID" ref="true" disabled></v-text-field>
+      <v-text-field
+          v-model="localForm.title"
+          :counter="255"
+          label="Titel"
+          required></v-text-field>
 
-    <template #top>
-      <v-toolbar flat color="blue-lighten-4">
-        <v-toolbar-title>Bücher</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn icon color="primary" dark @click="createDialog">
-          <v-icon>mdi-plus</v-icon>
+      <v-text-field
+          v-model="localForm.author"
+          :counter="255"
+          label="Autor"
+          required></v-text-field>
+
+      <v-text-field
+          v-model="localForm.isbn"
+          :counter="255"
+          label="ISBN"
+          required></v-text-field>
+
+      <v-text-field
+          v-model="localForm.description"
+          :counter="255"
+          label="Beschreibung"
+          required></v-text-field>
+
+      <v-text-field
+          v-model="localForm.publisher"
+          :counter="255"
+          label="Herausgeber"
+          required></v-text-field>
+
+      <v-text-field
+          v-model="localForm.publishedDate"
+          :counter="255"
+          label="Veröffentlichungsdatum"
+          required></v-text-field>
+
+      <div class="d-flex flex-column">
+        <v-btn color="primary" class="mt-4" block @click="isEditing ? modify() : create()">
+          {{ isEditing ? 'Speichern' : 'Erstellen' }}
         </v-btn>
-      </v-toolbar>
-      <v-dialog v-model="dialog" max-width="600px" @click:outside="closeDialog(value)">
-        <Book :form="selectedItem" @close-form="closeDialog(value)"/>
-      </v-dialog>
-    </template>
+        <v-btn class="custom-button mt-4" color="success" block @click="close">Abbrechen</v-btn>
 
-    <template #item.action="{ item }">
-      <v-btn icon color="blue-lighten-4" @click.stop="addToCart(item)">
-        <v-icon color="primary">mdi-basket</v-icon>
-      </v-btn>
-    </template>
-  </v-data-table>
+        <v-btn v-if="isEditing" color="error" class="mt-4" block @click="remove()">Löschen</v-btn>
+        <v-btn v-if="!isEditing" color="orange" class="custom-button mt-4" block @click="reset">Formular Zurücksetzen</v-btn>
+
+      </div>
+    </v-form>
+  </v-sheet>
 </template>
 
-<script setup lang="ts">
-import { computed, onMounted, reactive, ref } from 'vue'
-import { useBookStore } from "@/stores/common"
-import { useRouter } from "vue-router"
-import type { BookDTO } from "@/types";
+<script setup type="ts">
+import {computed, onMounted, reactive} from "vue";
+import {useBookStore, useSeriesStore} from "@/stores/common"
 
-const router = useRouter()
 const store = useBookStore()
-
-const items = computed(() => store.gets)
-
-const emptyBookDTO = (): BookDTO => ({
-  id: null,
-  title: null,
-  author: null,
-  isbn: null,
-  description: null,
-  publisher: null,
-  publishedDate: null,
-});
-
-let selectedItem = reactive(emptyBookDTO())
-let loading = ref(true)
-const search = ref('')
-let dialog = ref(false)
-let cart = ref([])
+const seriesStore = useSeriesStore()
+const series = computed(() => seriesStore.get)
+const props = defineProps({
+  form: {
+    type: Object,
+    default: () => ({ id: null,
+      title: null,
+      author: null,
+      isbn: null,
+      description: null,
+      publisher: null,
+      publishedDate: null}),
+    required: false
+  },
+})
+let localForm = reactive({ ...props.form });
+const isEditing = computed(() => localForm.id !== undefined && localForm.id !== null)
+const emit = defineEmits(['close-form'])
 
 onMounted(async () => {
   await fetchData()
-  created()
 })
 
 async function fetchData() {
   try {
     await store.init()
-    loading.value = false
-  } catch (e) {
-    console.error('Store error:', e)
+    await seriesStore.init()
+  } catch (error) {
+    console.error('Error fetching data:', error)
   }
 }
 
-const headers = ref([
-  { title: 'Id', key: 'id' },
-  { title: 'Titel', key: 'title' },
-  { title: 'Autor', key: 'author' },
-  { title: 'Isbn', key: 'isbn' },
-  { title: 'Herausgeber', key: 'publisher' },
-  { title: 'Beschreibung', key: 'description' },
-  { title: 'Veröffentlichungsdatum', key: 'publishedDate' },
-  { title: 'Aktion', value: 'action', sortable: false }
-])
+async function create() {
+  await store.add({
+    title: localForm.title,
+    author:  localForm.author,
+    isbn:  localForm.isbn,
+    description:  localForm.description,
+    publisher:  localForm.publisher,
+    publishedDate:  localForm.publishedDate,
+  })
+  close()
+}
 
-function createDialog() {
-  selectedItem = reactive(emptyBookDTO())
-  dialog.value = true
+function close() {
+  emit('close-form', null)
+}
+
+function modify() {
+  store.change(localForm)
+  emit('close-form', null)
+}
+
+function remove() {
+  store.remove(localForm.id)
+  close()
 }
 
 function reset() {
-  selectedItem = reactive(emptyBookDTO())
-}
-
-function closeDialog() {
-  fetchData()
-  dialog.value = false
-  reset()
-}
-
-const goToDetails = (event: MouseEvent, item: any) => {
-  selectedItem = items.value.find(o => o.id === item.internalItem.raw.id)
-  dialog.value = true
-}
-
-function created() {
-  cart.value = JSON.parse(localStorage.getItem('cart')) || []
-}
-
-function addToCart(book: BookDTO) {
-  cart.value.push(book)
-  localStorage.setItem('cart', JSON.stringify(cart.value))
-}
-
-function removeFromCart(book: BookDTO) {
-  cart.value = cart.value.filter(item => item.id !== book.id)
-  localStorage.setItem('cart', JSON.stringify(cart.value))
+  for (let key in localForm) {
+    localForm[key] = null
+  }
 }
 </script>
